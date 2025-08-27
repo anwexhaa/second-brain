6b3b26b5a0749a2960b0469ec29d79c24bcd7236
Anwesha Das <dasanwesha2409@gmail.com>
2025-08-27 07:42:34 +0530

    removed openai

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,5 @@
 import os
 from dotenv import load_dotenv
-import openai
-from openai import OpenAI
 from pathlib import Path
 
 env_path = Path(__file__).resolve().parent / ".env"
@@ -15,4 +13,3 @@ PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
 PINECONE_ENVIRONMENT = os.getenv("PINECONE_ENVIRONMENT")
 PINECONE_INDEX_NAME = os.getenv("PINECONE_INDEX_NAME")
 
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
